24b5602e4f1556727b7c475a10222c79efe0c8da
Mauricio Lima <mauricio_lima@globomail.com>
2017-06-08 19:24:47 -0300

    correcao do erro

--- megasena.c
+++ megasena.c
@@ -6,30 +6,6 @@
 #define LIN 5000
 #define COL 6
 
-void sorteio(int *concurso[LIN][COL]){
-    int c, l, i, j, num, igual=0;
-
-    srand(time(NULL));
-
-    for(l=0;l<LIN;l++){
-        for(c=0;c<COL;){
-            while(igual!=1){
-               num = rand()%61;
-               for(i=0;i<6;i++){
-                    if(num == concurso[l][i] || num == 0){
-                        igual=1;
-                    }
-               }//for i
-               if(igual==0){
-                concurso[l][c]=num;
-                c++;
-               }
-            }//while
-            igual=0;
-        }
-    }
-
-}//sorteio
 
 void bubleSort(int *vetor[LIN][COL]){
     int i, j, k, aux;
@@ -48,8 +24,8 @@ void bubleSort(int *vetor[LIN][COL]){
 
 }//ordenaSorteio
 
-void estatistica(int dezenas[], int matriz[LIN][COL]){
-    int i /*dezenas[60]*/;
+void estatistica(int *dezenas[], int *matriz[LIN][COL]){
+    int i;
     int l, c, num;
 
     //zerar o vetor
@@ -64,9 +40,15 @@ void estatistica(int dezenas[], int matriz[LIN][COL]){
             dezenas[num-1]++;
         }
     }
+    /*IMPRIME A ESTATISTICA DE NUMEROS SORTEADOS*/
+    printf("\n\n---- QUANTIDADE DE VEZES QUE UM NUMERO SAIU ----\n\n");
+    for(i=0;i<60;i++){
+        printf("Numero: %02d = %2d vezes\n",i+1, dezenas[i]);
+    }
+    printf("\n\n");
 }//estatistica
 
-void listarDuplas(int matriz[LIN][COL]){
+void listarDuplas(int *matriz[LIN][COL]){
     int i, j, l, c, e, num1=0, num2=0, duplas[61][61], cont, aux1, aux2, aux3;
     struct numeros{
         int numero1;
@@ -126,7 +108,7 @@ void listarDuplas(int matriz[LIN][COL]){
     }
 
     /*IMPRIME A STRUCT*/
-    printf("---- DUPLAS QUE MAIS SAIRAM ----\n\n");
+    printf("---- AS 15 DUPLAS QUE MAIS SAIRAM ----\n\n");
     for(i=1769;i>1754;i--){
         printf("Numeros %02d e %02d = %d vezes.\n", maisSorteados[i].numero1, maisSorteados[i].numero2, maisSorteados[i].quant);
     }
@@ -134,7 +116,7 @@ void listarDuplas(int matriz[LIN][COL]){
 
 }//LISTAR DUPLAS
 
-void numerosUnicos(int vetor[LIN][COL]){
+void numerosUnicos(int *vetor[LIN][COL]){
     int i, j, c, l, num, numMaisSorteados[61], maissort=0, aux1, aux2;
     struct numeros
     {
@@ -184,7 +166,7 @@ void numerosUnicos(int vetor[LIN][COL]){
     printf("\n\n");
 }
 
-void quantidadeJogadas(int vetor[LIN][COL]){
+void quantidadeJogadas(int *vetor[LIN][COL]){
     int i, j, l, c, num[61], saiu=0;
 
     /*ZERA O VETOR NUM*/
@@ -211,6 +193,7 @@ void quantidadeJogadas(int vetor[LIN][COL]){
         }
     }//FOR I
     /*IMPRESSAO NUMEROS QUE SAIRAM NA QUANT JOGADAS*/
+    printf("\n\n---- QUANTIDADE DE JOGADAS QUE UM NUMERO DEMOROU A SAIR ----\n\n");
     for(i=1;i<=60;i++){
         if(num[i]==0){
             printf("N° %02d = saiu no ultimo sorteio.\n");
@@ -224,61 +207,83 @@ void quantidadeJogadas(int vetor[LIN][COL]){
 
 int main() {
     int l, c, i, op=0;
+    int j, num, igual=0;
     int concurso[LIN][COL];
     int dezenas[60];
 
      setlocale(LC_ALL, "Portuguese");//habilita a acentuação para o português
 
     /*SORTEIA OS NUMEROS*/
-    sorteio(concurso);
-    bubleSort(concurso);
+        srand(time(NULL));
 
+    for(l=0;l<LIN;l++){
+        for(c=0;c<COL;){
+            while(igual!=1){
+               num = rand()%61;
+               for(i=0;i<6;i++){
+                    if((num == concurso[l][i]) || (num == 0)){
+                        igual=1;
+                    }
+               }//for i
+               if(igual==0){
+                concurso[l][c]=num;
+                c++;
+               }
+            }//while
+            igual=0;
+        }
+    }
+    /*FIM SORTEIA OS NUMEROS*/
+    bubleSort(concurso); //ORDENA OS NUMEROS NO SORTEIO
+    /*MENU*/
     do{
-        system("cls");
-        printf("Menu Principal\n\n");
-        printf("1 - IMPRIMIR SORTEIOS\n");
-        printf("2 - ESTATISTICAS DE NUMEROS\n");
-        printf("3 - LISTAR 15 DUPLAS QUE MAIS SAIRAM\n");
-        printf("4 - LISTAR OS 15 NUMEROS MAIS SORTEADOS\n");
-        printf("5 - LISTAR QUANT VEZES QUE NUMERO SAIU NAS JOGADAS\n");
-        scanf("%d", &op);
-    }while(op<1 || op>5);
+        do{
+            system("cls");
+            printf("Menu Principal\n\n");
+            printf("1 - IMPRIMIR SORTEIOS\n");
+            printf("2 - LISTAR QUANTAS VEZES CADA NUMERO SAIU\n");
+            printf("3 - LISTAR 15 DUPLAS QUE MAIS SAIRAM NOS SORTEIOS\n");
+            printf("4 - LISTAR OS 15 NUMEROS UNICOS QUE MAIS SAIRAM NOS SORTEIOS\n");
+            printf("5 - QUANTIDADE DE JOGADAS DESDE A ULTIMA VEZ QUE UM NUMERO FOI SORTEADO\n");
+            printf("6 - LISTAR TODAS AS ESTATISTICAS\n");
+            printf("0 - SAIR\n");
+            scanf("%d", &op);
+        }while(op<0 || op>6);
 
-    switch(op){
-        case 1:
-            for(l=0;l<LIN;l++){
-                for(c=0;c<COL;c++){
-                    printf("|%02d| ",concurso[l][c]);
+        switch(op){
+            case 1:
+                for(l=0;l<LIN;l++){
+                    for(c=0;c<COL;c++){
+                        printf("|%02d| ",concurso[l][c]);
+                    }
+                    printf("\n\n");
                 }
-                printf("\n\n");
-            }
-            listarDuplas(concurso);
-            break;
-        case 2:
-            estatistica(dezenas, concurso);
-            for(i=0;i<60;i++){
-                printf("Numero: %02d = %2d vezes\n",i+1, dezenas[i]);
-            }
-            break;
-        case 3:
-            listarDuplas(concurso);
-            break;
-        case 4:
-            numerosUnicos(concurso);
-            break;
-        case 5:
-            /*IMPRIME O SORTEIO*/
-            for(l=0;l<LIN;l++){
-                for(c=0;c<COL;c++){
-                    printf("|%02d| ",concurso[l][c]);
-                }
-                printf("\n\n");
-            }
+                break;
+            case 2:
+                estatistica(dezenas, concurso);
+                break;
+            case 3:
+                listarDuplas(concurso);
+                break;
+            case 4:
+                numerosUnicos(concurso);
+                break;
+            case 5:
+                quantidadeJogadas(concurso);
+                break;
+            case 6:
+                estatistica(dezenas, concurso);
+                listarDuplas(concurso);
+                numerosUnicos(concurso);
+                quantidadeJogadas(concurso);
+        }//switch
+        if(op!=0){
+            getch();
+        }
 
-            quantidadeJogadas(concurso);
-            break;
-    }//switch
+    }while(op!=0);
 
-    system("pause");
+    printf("\nfim do programa\n");
     return 0;
+
 }
